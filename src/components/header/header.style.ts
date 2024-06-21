a0892479752ae74b9adaa8ac57fa0ec57cebe652
JoaoVitor <joaovitor2201@hotmail.com>
2024-06-20 21:45:27 -0300

    Adicionando estilizacao da parte do header

--- src/components/header/header.style.ts
+++ src/components/header/header.style.ts
@@ -1,6 +1,15 @@
 import { styled } from "styled-components";
 
-export const BoldTypograph = styled.p`
+export const BoldTypograph = styled.b`
+  color: #45505E;
+  font-size: 18px;
   font-weight: 500;
   line-height: 21px;
+`;
+
+export const Description = styled.p`
+  color: #8C9CAD;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 16px;
 `;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,30 @@
 import logo from "../../assets/Logo.png";
-import { BoldTypograph } from "./header.style";
+import { BoldTypograph, Description } from "./header.style";
 import { useHeader } from "./useHeader";
 
 const Header = () => {
   const { setTeste, teste } = useHeader();
   return (
     <header>
-      <h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          margin: "64px 0 0 64px",
+        }}
+      >
         <a href="google.com">
           <img src={logo} alt="logo" />
         </a>
-      </h1>
-      <button onClick={() => setTeste("Teste secundario")}>Botao teste</button>
-      <BoldTypograph>{teste}</BoldTypograph>
+        <div style={{ marginLeft: "50px" }}>
+          <BoldTypograph>
+            14 de janeiro<span style={{ margin: "0 15px" }}>|</span>21:00 UTC
+          </BoldTypograph>
+          <Description>
+            Dados de câmbio disponibilizados pela Morningstar.
+          </Description>
+        </div>
+      </div>
     </header>
   );
 };
